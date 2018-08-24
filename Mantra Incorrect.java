9ca77d3691eff7c930015faccdbc95b5ae77c755
AlvaroFdP <42589672+AlvaroFdP@users.noreply.github.com>
2018-08-24 00:00:18 -0700

    Add files via upload

---
+++ Mantra Incorrect.java
@@ -0,0 +1,14 @@
+public class Mantra{
+    public static void main(String args[]){
+       
+        mustKnow();
+        mustKnow();
+        
+}     
+    public void mustKnow(){
+        System.out.println("There's one thing every coder must understand:");
+        System.out.println("The System.out.println command.\n");
+
+          
+      }        
+}
